d6d257f6e6a70c4a89e9600057df2794af0e8e96
ashishpatel26 <shriganesh.patel@gmail.com>
2023-12-01 13:11:44 +0530

    Add settings for todo-tree and chatbot functionality

--- inject.py
+++ inject.py
@@ -1,18 +1,25 @@
-from langchain.document_loaders import PyPDFLoader
+from langchain.document_loaders import PyMuPDFLoader
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import FAISS
 
+# TODO: 
+# [X]1. Document loaders
+# [X]2. Text Splitter
+# [X]3. Embeddings
+# [X]4. Vector stores
+
 class DocLoad:
     """The `DocLoad` class is responsible for loading and splitting PDF documents."""
 
-    def __init__(self, file_path: str = None):
+    def __init__(self, file_path = None):
         """
         Constructor that initializes an object with a file path.
 
         Args:
             file_path (str): String representing the path to a file.
         """
+        # COMMENT: Initialize the object with a file path
         self.file_path = file_path
         self.loader = None
         self.document = None
@@ -26,8 +33,9 @@ class DocLoad:
         Returns:
             Loaded document.
         """
+        # COMMENT: This function loads a PDF file using PyPDFLoader and returns the loaded document.
         print("[Status] Loading PDF...✅✅✅")
-        self.loader = PyPDFLoader(self.file_path)
+        self.loader = PyMuPDFLoader(self.file_path)
         self.document = self.loader.load()
         print("[Status] Loading PDF and split...✅✅✅")
         return self.document
@@ -36,6 +44,7 @@ class DocLoad:
         """
         Loads a PDF document, splits it into chunks of text, and returns the chunks.
         """
+        # COMMENT: This function is text splitting used for document
         if not self.document:
             self.load_pdf()
 
@@ -66,6 +75,7 @@ class VectorStore(FAISS):
         Returns:
             FAISS vector database.
         """
+        # COMMENT: This is FAISS vector store from documents and embeddings.
         self.vectordb = FAISS.from_documents(self.docs, self.embeddings)
         return self.vectordb
 
@@ -74,4 +84,5 @@ if __name__ == '__main__':
     doc = DocLoad(file_path="./data/tmp/tmp36hqd4ss.pdf")
     chunks = doc.split_text()
     vectordb = VectorStore(embeddings=OpenAIEmbeddings(), docs=chunks)
+
     
